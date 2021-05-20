6f27924231085291ff6cf1caffb3e13bbdaead6e
Fitrah Arie Ramadhan <56785130+Fitrah1812@users.noreply.github.com>
2021-05-20 19:42:19 +0700

    Update client.c

--- soal1/Client/client.c
+++ soal1/Client/client.c
@@ -1,291 +1,168 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
 #include <string.h>
+// #include <cstring>
+#include <math.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/sendfile.h>
-#include <fcntl.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <pthread.h>
-#include <wait.h>
-#include <time.h>
-#define maxi 1024
-#define PORT 8080
+#include <stdlib.h>
+#include <stdbool.h>
+#define cuk 0.001
+#define gas 512-(212*1)
 
+const int cekisi = sizeof(char) * gas;
+const int ceksize = sizeof(int) * gas;
+const int input = sizeof(int)* gas * 1;
 
-typedef struct mamang
+//pembuatan socket server
+int create_socket()
 {
-    char id[maxi];
-    int check;
-    char mantap[maxi];
-   
-}memeng;
-typedef struct cekbuku
-{
-    char publisher[maxi];
-    char path[maxi];
-    int tahun;
-   
-}sitasi;
-typedef struct loginaja
-{
-    char id[maxi];
-    int status;
-    char password[maxi];
-    
-}login;
-void menuutama(){
-    puts("Selamat datang di Aplikasi ini");
-    puts("Berikut merupakan pilihan aksi yang diberikan");
-    puts("1. Melakukan Registrasi");
-    puts("2. Melakukan login");
-    printf("Silahkan memasukkan pilihan yang sudah tersedia = ");
+    struct sockaddr_in saddr;
+    int fd, ret_val;
+    int opt = 1;
+    struct hostent *local_host; 
+    fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (fd == -1) {
+        fprintf(stderr, "socket failed [%s]\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(7000);
+    local_host = gethostbyname("127.0.0.1");
+    saddr.sin_addr = *((struct in_addr *)local_host->h_addr);
+    ret_val = connect(fd, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
+    if (ret_val == -1) {
+        fprintf(stderr, "connect failed [%s]\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    return fd;
 }
-void ambilusername(){
-    printf("Masukkan username = ");
+char command[gas];
+bool cek = false;
+bool posisi = true;
+bool cuek = false;
+//cek input
+//cek input tersebut sudah oke atau belum
+void *cekinput(void *fdc)
+{
+    chdir("/home/fitraharie/soal1/Client");
+    int fd = *(int *) fdc;
+    char message[gas] = {0};
+
+    while (1) {
+        gets(message);
+        send(fd, message, cekisi, 0);
+        if (cek) {
+            strcpy(command, message);
+        }
+    }
+}
+void activeserver(int fd, char *input)
+{
+    if (recv(fd, input, gas, 0) == 0) {
+        printf("Server telah meninggal\n");
+        exit(EXIT_SUCCESS);
+    }
+}
+//cek apakah output tersebut sesuai atau tidak
+void *cekoutput(void *fdc) 
+{
+    //change directory 
+    chdir("/home/fitraharie/soal1/Client");
+    int fd = *(int *) fdc;
+    char message[gas] = {0};
+
+    while (1) 
+    {
+        //set pesan menjadi nol
+        memset(message, 0, cekisi);
+        activeserver(fd, message);
+        printf("%s", message);
+        //pindahkan path
+        if (strcmp(message, "Filepath: ") == 0) {
+            cek = true;
+        } else if (strcmp(message, "Memulai mengirimkan file\n") == 0) {
+            kirim(fd);
+            cek = false;
+        } else if (strcmp(message, "File yang anda upload sudah ada\n") == 0) {
+            cek = false;
+        } else if (strcmp(message, "Memulai menerima file\n") == 0) {
+            cetak(fd);
+        } 
+        fflush(stdout);
+    }
+}
+//send file dari client to server
+void kirim(int fd)
+{
+    //jangan lupa send pesan
+    printf("Mengirimkan file %s ke server\n", command);
+    int ret_val;
+    FILE *sends = fopen(command, "r");
+    char buf[gas] = {0};
+
+    if (sends) {
+        //file ditemukan
+        send(fd, "File ditemukan", cekisi, 0);
+        fseek(sends, 0L, SEEK_END);
+        int size = ftell(sends);
+        rewind(sends);
+        sprintf(buf, "%d", size);
+        send(fd, buf, cekisi, 0);
+        while ((ret_val = fread(buf, 1, 1, sends)) > 0) 
+        {
+            send(fd, buf, 1, 0);
+        } 
+        printf("File telah dikirimkan\n");
+        fclose(sends);
+    } 
+    else 
+    {
+        printf("File tidak ada\n");
+        send(fd, "File tidak ada", cekisi, 0);
+    }
 }
 
-void menusukseslogin(){
-    puts("Selamat datang di halaman utama");
-    puts("Silahkan memilih aksi yang diinginkan (add/download/delete/see/find)");
-    printf("Jawaban = ");
-}
-void ambilpassword(){
-    printf("Masukkan Password = ");
+void cetak(int fd)
+{
+    char buff[gas] = {0};
+    int ret_val = recv(fd, buff, gas, 0);
+    FILE *sends = fopen(buff, "w+");
+    recv(fd, buff, gas, 0);
+    int size = atoi(buff);
+    while (size > 0) {
+        ret_val = recv(fd, buff, gas, 0);
+        //write 
+        fwrite(buff, 1, ret_val, sends);
+        memset(buff, 0, cekisi);
+        size -= ret_val;
+    }
+    //masukkan ke client
+    puts("File berhasil dikirimkan ke server");
+    //send to server
+    send(fd, "File berhasil dikirimkan", cekisi, 0);
+    fclose(sends);
 }
 int main(int argc, char const *argv[])
 {
-	int sock = 0, valread;
-    int cekloginbrow = 0;
-    login login1;
-	struct sockaddr_in serv_addr;
-	char buffer[1024] = {0};
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("\n Socket creation error \n");
-		return -1;
-	}
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
-	// if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-	// {
-	// 	printf("\nInvalid address/ Address not supported \n");
-	// 	return -1;
-	// }
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	{
-		printf("\nConnection Failed \n");
-		return -1;
-	}
-    //cek perulangan
-
-    while (1)
-    {
-        if (!cekloginbrow)
-        {
-            char username1[maxi];
-            char password1[maxi];
-            char message[maxi];
-            int cek;
-            char buffer[maxi];
-            int flag;
-            menuutama();
-            scanf("%d",&cek);
-            login1.status = cek-1;
-            if(cek == 1)
-            {
-                //cek apabila sudah sesuai
-                ambilusername();
-                scanf("%s",buffer);
-                //copas setiap akun
-                strcpy(login1.id,buffer);
-                ambilpassword();
-                //cek password
-                scanf("%s",buffer);
-                strcpy(login1.password,buffer);
-                int snd = send(sock,(void*)&login1,sizeof(login1),0);
-                if (flag = recv(sock,(void*)&message,sizeof(message),0) < 0)
-                {
-                    perror("Register gagal");
-                    exit(EXIT_FAILURE);
-                }
-                cekloginbrow = 1;
-            }
-            else if(cek == 2)
-            {
-                //login
-                char usernameusername[maxi];
-                char usernamepassword[maxi];
-                printf("Masukkan Username =  ");
-                scanf("%s", buffer);
-                printf("Masukkan Password = ");
-                scanf("%s", buffer);
-                if (flag = recv(sock,(void*)&message,sizeof(message),0) < 0)
-                {
-                    perror("Gagal membuat akun");
-                    exit(EXIT_FAILURE);
-                }
-                cekloginbrow=1;
-            } 
-        }
-        else
-        {
-            int flag1;
-            menusukseslogin();
-            char pilihan[1024];
-            scanf("%s", pilihan);
-            if (flag1 = send(sock,(void*)&pilihan,sizeof(pilihan),0) < 0)
-            {
-                perror("Sedang Mengirimkan");
-                exit(EXIT_FAILURE);
-            }
-            if(strcmp(pilihan, "add") == 0)
-            {
-                sitasi lah;
-                int flag2;
-                login1.status = 3;
-                printf("Publisher: ");
-                scanf("%s",lah.publisher);
-                char message[1024]= {};
-                printf("Tahun Publikasi: ");
-                scanf("%d",&lah.tahun);
-
-                printf("path: ");
-                scanf("%s",lah.path);
-
-                char pesan[maxi];
-                int pesanditerima;
-
-                if (flag2 = send(sock,(void*)&lah,sizeof(lah),0) < 0)
-                {
-                    perror("Send add");
-                    exit(EXIT_FAILURE);
-                }
-                if (pesanditerima = recv(sock,(void*)&message,sizeof(message),0) < 0)
-                {
-                    perror("Add Failed");
-                    exit(EXIT_FAILURE);
-                }
-                printf("%s\n",message);
-            }
-            else if (strcmp(pilihan, "download") == 0)
-            {
-                char download[1024]= {};
-                int senddownload;
-                char message[1024];
-                int pesanditerima;
-
-                printf("Masukkan nama file yang ingin didownload = ");
-                scanf("%s",download);
-                if (senddownload = send(sock,(void*)&download,sizeof(download),0) < 0)
-                {
-                    perror("Download sudah dikirimkan");
-                    exit(EXIT_FAILURE);
-                }
-                if (pesanditerima = recv(sock,(void*)&message,sizeof(message),0) < 0)
-                {
-                    perror("Download Failed");
-                    exit(EXIT_FAILURE);
-                }
-                printf("%s\n",message);
-            }
-            else if (strcmp(pilihan, "delete") == 0)
-            {
-                char delete[maxi];
-                int del;
-                char message[maxi];
-                int pesanditerima;
-                printf("Nama file yang akan dihapus: ");
-                scanf("%s",delete);
-                if (del = send(sock,(void*)&delete,sizeof(delete),0) < 0)
-                {
-                    //cek error
-                    perror("Mengirimkan delete");
-                    exit(EXIT_FAILURE);
-                }
-                if (pesanditerima = recv(sock,(void*)&message,sizeof(message),0) < 0)
-                {
-                    perror("download_gagal");
-                    exit(EXIT_FAILURE);
-                }
-                printf("%s\n",message);
-            }
-            else if (strcmp(pilihan, "find") == 0)
-            {
-                printf("Masukkan Pola kata: ");
-                char pola[maxi];
-                sitasi savede[maxi];
-                int fndMsg;
-                int sendFnd;
-                scanf("%s",pola);
-                if (sendFnd = send(sock,(void*)&pola,sizeof(pola),0) < 0)
-                {
-                    perror("Mengirimkan find");
-                    exit(EXIT_FAILURE);
-                }
-                if (fndMsg = recv(sock,(void*)&savede,sizeof(savede),0) < 0)
-                {
-                    perror("cek gagal");
-                    exit(EXIT_FAILURE);
-                }
-                //cek satu persatu
-                for (int i=0; i<maxi; i++)
-                {
-                    if (savede[i].tahun == 0)
-                    {
-                        break;
-                    } 
-                    sitasi rd = savede[i];
-                    char cekking[maxi];
-                    strcpy(cekking,rd.path);
-                    char* oker = strrchr(cekking,'/');
-                    //remove
-                    memmove(&oker[0],&oker[1],strlen(oker) - 0);
-                    //print nama
-                    printf("%s\n",oker);
-                }
-            }
-            else if (strcmp(pilihan, "see") == 0)
-            {
-                sitasi savede[maxi];
-                int hore;
-                if (hore = recv(sock,(void*)&savede,sizeof(savede),0) < 0)
-                {
-                    perror("Aksi see error");
-                    exit(EXIT_FAILURE);
-                }
-                for (int i=0 ; i<maxi; i++)
-                {
-                    sitasi rd = savede[i];
-                    char ceke[maxi];
-                    char buff2[maxi];
-                    char* cekes = strrchr(buff2,'.');
-                    char* oker = strrchr(ceke,'/');
-                    
-                    
-                    strcpy(ceke,rd.path);
-                    if (savede[i].tahun == 0){
-                        break;
-                    }
-                    memmove(&oker[0],&oker[1],strlen(oker) - 0);
-                    printf("Nama: %s\n",oker);
-
-                    printf("Publisher: %s\n",savede[i].publisher);
-
-                    printf("Tahun Publishing: %d\n",savede[i].tahun);
-                    strcpy(buff2,oker);
-
-                    memmove(&cekes[0],&cekes[1],strlen(cekes) - 0);
-
-                    printf("Ekstensi File: %s\n",cekes);
-
-                    printf("Filepath: %s\n",savede[i].path);
-
-                    puts("");
-                }
-            }
-        }
-    }
-	return 0;
+    pthread_t tid[2];
+    int fdc = create_socket();
+    //bisa menghandle client banyak
+    //minta masukka username
+    pthread_create(&(tid[0]), NULL, &cekoutput, (void *) &fdc);
+    //minta masukkan email
+    pthread_create(&(tid[1]), NULL, &cekinput, (void *) &fdc);
+    //joinkan
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    close(fdc);
+    return 0;
 }
+
